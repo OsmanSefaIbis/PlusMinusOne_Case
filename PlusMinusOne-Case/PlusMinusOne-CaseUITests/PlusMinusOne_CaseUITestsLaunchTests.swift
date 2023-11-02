c101b0c72004de04e642cfd83b26842e2056ed19
Sefa IBIS <osmansefaibis@gmail.com>
2023-11-02 20:25:08 +0300

    Initiated with project boilerplate

---
+++ PlusMinusOne-Case/PlusMinusOne-CaseUITests/PlusMinusOne_CaseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PlusMinusOne_CaseUITestsLaunchTests.swift
+//  PlusMinusOne-CaseUITests
+//
+//  Created by Sefa İbiş on 2.11.2023.
+//
+
+import XCTest
+
+final class PlusMinusOne_CaseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
